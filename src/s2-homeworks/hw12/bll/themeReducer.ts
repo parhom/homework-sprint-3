03b9b0e34b2a026d5e4397ad521fd4a064b9d250
Anton Parkhomchuk <anton.parhom@gmail.com>
2026-01-07 05:21:35 +0200

    HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,24 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ThemeIdActionType): changeThemeIdType => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":{
+            return {themeId: action.id}
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ThemeIdActionType => ({ type: 'SET_THEME_ID', id }) // fix any
+
+export type changeThemeIdType = {
+    themeId: number
+}
+
+export type ThemeIdActionType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
